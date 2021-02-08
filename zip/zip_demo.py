07ddbb1e7ae855f01eb4ceff936e55e51b2ca895
Mladen Uzelac <mlauzela@inet.hr>
2021-02-08 23:09:58 +0100

    compressing a zipfile

--- zip/zip_demo.py
+++ zip/zip_demo.py
@@ -1,6 +1,6 @@
 import zipfile 
 
-with zipfile.ZipFile('files.zip', 'w') as my_zip:
+with zipfile.ZipFile('files.zip', 'w', compression=zipfile.ZIP_DEFLATED) as my_zip:
 
     my_zip.write('test.txt')
     my_zip.write('bier.jpg')
