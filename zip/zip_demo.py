73f17d612c90bfeb0f19f298f824d2821467b4bd
Mladen Uzelac <mlauzela@inet.hr>
2021-02-08 23:13:10 +0100

    get the name of the files of the zipped files

--- zip/zip_demo.py
+++ zip/zip_demo.py
@@ -1,6 +1,10 @@
 import zipfile 
 
-with zipfile.ZipFile('files.zip', 'w', compression=zipfile.ZIP_DEFLATED) as my_zip:
+# with zipfile.ZipFile('files.zip', 'w', compression=zipfile.ZIP_DEFLATED) as my_zip:
+# 
+#     my_zip.write('test.txt')
+#     my_zip.write('bier.jpg')
 
-    my_zip.write('test.txt')
-    my_zip.write('bier.jpg')
+
+with zipfile.ZipFile('files.zip', 'r') as my_zip:
+    print(my_zip.namelist())
